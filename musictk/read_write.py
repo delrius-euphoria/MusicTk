b8c4a4e2dc45e8be0408eca59e639f8aba176a3a
Raphael-CV <raphael.dechevron@gmail.com>
2022-01-21 01:23:44 +0100

    Modification of read_write.py to respect PEP8

--- musictk/read_write.py
+++ musictk/read_write.py
@@ -1,8 +1,11 @@
-import appdirs
-import toml
 import os
 
+import appdirs
+import toml
+
+
 appname = 'MusicTk'
+
 
 def create():
     try:
@@ -10,12 +13,13 @@ def create():
         os.makedirs(directory)
     except FileExistsError:
         pass
-    
+
     return directory
+
 
 def write():
     path = appdirs.user_data_dir(appname)
-    
+
     toml_string = """
 [app]
 app = "MusicTk"
@@ -28,32 +32,36 @@ app = "MusicTk"
     ismini = false
 """
     parsed_toml = toml.loads(toml_string)
-    with open(path+'/settings.toml', 'w') as f:
+    with open(path + '/settings.toml', 'w', encoding='utf-8') as f:
         toml.dump(parsed_toml, f)
+
 
 def read():
     path = appdirs.user_data_dir(appname)
     try:
-        toml.load(path+'/settings.toml')['app']['settings']
+        toml.load(path + '/settings.toml')['app']['settings']
     except KeyError:
         write()
-    
-    app = toml.load(path+'/settings.toml')['app']
+
+    app = toml.load(path + '/settings.toml')['app']
     settings = app['settings']
-    
-    return settings['theme'],settings['path'],settings['last_song_idx'],settings['last_song_name'],settings['ismini'] 
-    
-def save(theme,path,idx,name,ismini):
+
+    return settings['theme'], settings['path'], settings['last_song_idx'], \
+        settings['last_song_name'], settings['ismini']
+
+
+def save(theme, path, idx, name, ismini):
     path_dir = appdirs.user_data_dir(appname)
-    
-    data = toml.load(path_dir+'/settings.toml')
+
+    data = toml.load(path_dir + '/settings.toml')
     app = data['app']
     settings = app['settings']
-    settings['theme']  = theme
+    settings['theme'] = theme
     settings['ismini'] = ismini
     settings['last_song_idx'] = idx
-    settings['last_song_name'] = name   
-    if path: settings['path'] = path
+    settings['last_song_name'] = name
+    if path:
+        settings['path'] = path
 
-    with open(path_dir+'/settings.toml','w',encoding='utf-8') as f:
-        toml.dump(data,f)   
+    with open(path_dir + '/settings.toml', 'w', encoding='utf-8') as f:
+        toml.dump(data, f)
